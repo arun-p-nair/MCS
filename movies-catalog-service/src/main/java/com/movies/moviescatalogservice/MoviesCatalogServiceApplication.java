7325f41c0ef9e8789e498738f4712b070f825884
Arun <arun.p.nair@outlook.com>
2021-09-02 22:02:30 +0530

    Initial commit for creating the project

---
+++ movies-catalog-service/src/main/java/com/movies/moviescatalogservice/MoviesCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.movies.moviescatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesCatalogServiceApplication.class, args);
+	}
+
+}
